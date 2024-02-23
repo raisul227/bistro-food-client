be5fc29e744ad789e72e327ed3edd27b0166ee85
raisul227 <raisul2277@gmail.com>
2024-02-23 23:42:06 +0600

    fix navbar and footer and reduce image size

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -14,8 +14,7 @@ const Navbar = () => {
     }
 
     const navLinks = <>
-        <li><Link>Home</Link></li>
-        <li><Link>Contact Us</Link></li>
+        <li><Link to='/'>Home</Link></li>
         <li><Link to={isAdmin ? `/dashboard/adminHome` : `/dashboard/userHome`}>DashBoard</Link></li>
         <li><Link to='/menu'>Our Menu</Link></li>
         <li><Link to='/order/salad'>Our Order</Link></li>
